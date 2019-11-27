0860b0b3ce61ceea064e93fe6d2be3974d91b786
benonimonteiro <benonimonteirog@gmail.com>
2019-11-26 21:17:27 -0300

    SellerDaoJDBC.insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import dao.DaoFactory;
@@ -20,16 +21,19 @@ public class Program {
 		System.out.println("\n=== TEST 2: Seller.findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
 		System.out.println("\n=== TEST 3: Seller.findAll ===");
 		list = sellerDao.findAll();
-		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: Seller.insert ===");
+		seller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(seller);
+		System.out.println("Inserted! New id = " + seller.getId());
 	}
 }
--- src/dao/SellerDaoJDBC.java
+++ src/dao/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,13 +24,47 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 	
 	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+	public void insert(Seller seller) {
+		PreparedStatement st = null;
 		
+		try {
+			String sql = "INSERT INTO seller "
+						+ "(NAME, EMAIL, BIRTH_DATE, BASE_SALARY, DEPARTMENT_ID) "
+						+ "VALUES "
+						+ "(?, ?, ?, ?, ?)";
+			
+			st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+			st.setDouble(4, seller.getBaseSalary());
+			st.setInt(5, seller.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					seller.setId(id);
+				}
+				Db.closeResultSet(rs);
+			} 
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			Db.closeStatement(st);
+		}
 	}
 
 	@Override
-	public void update(Seller obj) {
+	public void update(Seller seller) {
 		// TODO Auto-generated method stub
 		
 	}
